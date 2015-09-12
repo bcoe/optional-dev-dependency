809fc6a85cf56e8ec364aa3ba3750a341f68d702
Benjamin Coe <ben@npmjs.com>
2015-09-12 13:49:15 -0700

    made the bin

---
+++ bin/odd.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+var yargs = require('yargs')
+  .usage('$0 package [options]')
+  .option('s', {
+    alias: 'silent',
+    default: false,
+    describe: 'should the `npm install` output be shown?'
+  })
+  .example('$0 lodash hiredis', "try to install 'lodash' and 'hiredis', it's okay if an install fails.")
+  .alias('h', 'help')
+  .version(require('../package').version, 'v')
+  .alias('v', 'version')
+  .help('h')
+var argv = yargs.argv
+var optionalDevDependency = require('../')
+
+if (argv._.length === 0) {
+  yargs.showHelp()
+} else {
+  optionalDevDependency(argv._, {
+    stdio: argv.silent ? null : 'inherit',
+    concurrency: argv.concurrency
+  })
+}
